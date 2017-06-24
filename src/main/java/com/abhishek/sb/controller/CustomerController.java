d46d5a1219249b3213c7e95a6df2a1769bd270cb
abhishekjawali <abhishek.jawali@emc.com>
2017-06-24 22:28:08 +0530

    implemented add customer func

--- src/main/java/com/abhishek/sb/controller/CustomerController.java
+++ src/main/java/com/abhishek/sb/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.abhishek.sb.controller;
 
+import com.abhishek.sb.domain.CustomerDomain;
 import com.abhishek.sb.entities.Customer;
 import com.abhishek.sb.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,7 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, value = "/list")
     public ResponseEntity<Collection<Customer>> getCustomers() {
         return new ResponseEntity<>(customerService.findAll(), HttpStatus.OK);
     }
@@ -27,5 +28,22 @@ public class CustomerController {
     @RequestMapping(method = RequestMethod.GET, value = "/{customerId}")
     public ResponseEntity<Customer> findOneCustomer(@PathVariable Long customerId) {
         return new ResponseEntity<>(customerService.findOne(customerId), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Customer>  createCustomer(CustomerDomain customerDomain) {
+        Customer customer = null;
+        try {
+            customer = customerService.createCustomer(customerDomain);
+        } catch(Exception e) {
+            System.out.println("error in creating customer");
+            e.printStackTrace();
+        }
+        if(null!= customer) {
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+        }
     }
 }
---
+++ src/main/java/com/abhishek/sb/domain/CustomerDomain.java
@@ -0,0 +1,28 @@
+package com.abhishek.sb.domain;
+
+/**
+ * Created by jawala on 6/24/2017.
+ */
+public class CustomerDomain {
+    public CustomerDomain() {
+    }
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
